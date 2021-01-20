ff990d28c8f2f6b612fbca164403aa5790360950
Latifah Lillie <queen1892@gmail.com>
2021-01-19 19:26:35 -0600

    loginnow

--- controllers/dogs_controller.js
+++ controllers/dogs_controller.js
@@ -1,19 +1,61 @@
-var db = require("../models");
+var express = require("express");
 
+var router = express.Router();
 
-module.exports = function (app) {
+// Import the model (cat.js) to use its database functions.
+var db = require("../models/");
 
-    app.get("/", function(req, res){
-            res.render("index")
-        })
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  db.all(function(data) {
+    var hbsObject = {
+      cats: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
 
-    app.get("/api/dogs", function (req, res) {
-        db.Dog.findAll({}).then(function (dbDog) {
-                res.json(dbDog);
-            });
-        })
+router.post("/api/cats", function(req, res) {
+  db.create([
+    "name", "sleepy"
+  ], [
+    req.body.name, req.body.sleepy
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
 
+router.put("/api/cats/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
 
+  console.log("condition", condition);
 
-
+  db.update({
+    sleepy: req.body.sleepy
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
     }
+  });
+});
+
+router.delete("/api/cats/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  db.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
